7380b3f2ce0a7503d8eae2c2626ed5f7554a3c9b
kikito <kikito@gmail.com>
2010-09-23 18:57:48 +0200

    after_create callback for custom field created. Small fixes

--- lib/custom_field_patch.rb
+++ lib/custom_field_patch.rb
@@ -2,18 +2,31 @@ require_dependency 'custom_field'
 
 module CustomFieldPatch
   def self.included(base) # :nodoc:
-    base.class_eval do
+    base.send(:include, InstanceMethods)
     
-      named_scope(:used_for_project_searching,
-        {:conditions => {:type => "ProjectCustomField", :field_format => 'list'}}
-      )
-      
-      named_scope(:used_for_project_filtering, lambda do |*args|
-        used_fields = Setting[:plugin_redmine_project_filtering]['project_filtering_used_fields'].keys.collect(&:to_i)
-        {:conditions  => ['custom_fields.id IN (?)', used_fields]}
+    base.class_eval do      
+      named_scope( :used_for_project_filtering, lambda do |*args|
+        used_fields = Setting[:plugin_redmine_project_filtering][:project_filtering_used_fields].keys.collect(&:to_i)
+        { :conditions  => [ "custom_fields.type = 'ProjectCustomField' 
+                             AND custom_fields.field_format = 'list' 
+                             AND custom_fields.id IN (?)", used_fields]}
       end)
-    
+      
+      after_create :configure_use_in_project_filtering
+      
     end
+  end
+  
+  module InstanceMethods
+  
+    def configure_use_in_project_filtering
+      if(self.type == 'ProjectCustomField' and field_format=='list')
+        value = Setting[:plugin_redmine_project_filtering][:project_filtering_used_fields]
+        value[self.id.to_s] = "1"
+        Setting[:plugin_redmine_project_filtering] = { :project_filtering_used_fields => value }
+      end
+    end
+  
   end
 end
 
