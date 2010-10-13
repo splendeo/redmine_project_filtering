f54c94fc3ba0f2d5190b16d99d51baa6e4ada5b8
Enrique García <kikito@gmail.com>
2010-10-13 18:22:28 +0200

    removed Project.roots named scope (was conflicting with awesome_nested_set and was unused

--- lib/patches/project_patch.rb
+++ lib/patches/project_patch.rb
@@ -4,10 +4,6 @@ module ProjectPatch
 
   def self.included(base) # :nodoc:
     base.send(:include, WithCustomValues)
-    
-    base.class_eval do
-      named_scope :roots, { :conditions => "projects.parent_id IS NULL" }
-    end
   end
 
 end
